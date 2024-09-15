58a1923f99a47a18ae416b02510d989a3b6ed478
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-15 15:12:10 -0600

    create marker

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from '@vis.gl/react-google-maps'
 
-const apiKey = import.meta.env.VITE_API_KEY;
+const apiKey = import.meta.env.VITE_MAPS_API_KEY;
+const mapsID = import.meta.env.VITE_MAPS_ID;
 
 function App() {
   
+const position = { lat: 53, lng: 10};
 
   return (
     <APIProvider apiKey={apiKey}>
-      <h1>test</h1>
+      
+      <div style={{height: "100vh"}}>
+        <Map zoom={9} center={position} mapId={mapsID}>
+          <AdvancedMarker position={position}></AdvancedMarker>
+        </Map>
+      </div>
+      
     </APIProvider>
   )
 }
