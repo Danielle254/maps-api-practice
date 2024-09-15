92f0d8da6100a7cd09b3d64d8d41d6b67eb30103
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-09-15 14:51:29 -0600

    store and call API key - vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
 import './App.css'
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from '@vis.gl/react-google-maps'
+
+const apiKey = import.meta.env.VITE_API_KEY;
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <APIProvider apiKey={apiKey}>
+      <h1>test</h1>
+    </APIProvider>
   )
 }
 
